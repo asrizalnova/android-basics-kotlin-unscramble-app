27fd2816e2fa95de60210d93c94d2197a03664ba
asrizalnova <asrizal7889@gmail.com>
2023-09-22 10:08:14 +0700

    Menambahkan properti pendukung ke currentScrambledWord

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,5 +5,8 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
     private var score = 0
     private var currentWordCount = 0
-    private var currentScrambledWord = "test"
+
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 }
